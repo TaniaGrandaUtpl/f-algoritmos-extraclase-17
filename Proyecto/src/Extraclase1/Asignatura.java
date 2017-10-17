2f204049628a3e4c84b98371ffb0e2142d43f1c0
TaniaGrandaUtpl <tfgranda@gmail.com>
2017-10-17 15:58:13 -0500

    Quinto commit

---
+++ Proyecto/src/Extraclase1/Asignatura.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Extraclase1;
+
+/**
+ *
+ * @author Usuario
+ */
+public class Asignatura {
+    private String nombre;
+    private int creditos;
+    
+    public void establecer_nombre(String nom){
+        nombre = nom;
+    }
+    public String obtener_nombre(){
+        return nombre;
+    }
+    
+    public void establecer_creditos(int cred){
+        creditos = cred;
+    }
+    public int obtener_creditos(){
+        return creditos;
+    }
+
+    @Override
+    public String toString() {
+        return nombre+creditos;
+    }
+    
+    
+    
+}
